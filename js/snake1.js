7587facf5a04166ae04155ef78833bca9dc9a03f
rywlln3344 <362510665@qq.com>
2020-09-25 15:30:53 +0800

    tanchishe

--- js/snake1.js
+++ js/snake1.js
@@ -1,6 +1,8 @@
 let canvas = document.getElementById('snake');
 let nanduxuanze = document.getElementById('nanduxuanze');
 let gameOver = document.getElementById('gameOver');
+let controlArea = document.getElementById('controlArea');
+let control = document.getElementById('control');
 let success = document.getElementById('success');
 let ctx = canvas.getContext('2d');
 let snakeLen = 6;
@@ -115,11 +117,11 @@ function moveFood(){
     addFood()
 }
 addFood()
-function gospeed(speed){
+function gospeed(speed,num){
     showEle('snake');
     noneEle('nanduxuanze','gameOver')
     let timerMove = setInterval(()=>{
-        if(snakeBody.length==230){
+        if(snakeBody.length==num){
             showGameSuccess()
         }
         if(status == 'right'){
@@ -192,3 +194,76 @@ function noneEle(...arg) {
         window[item].style.display = 'none'
     })
 }
+function keyDown(e){
+    if(e.keyCode==40 && status !='up'){
+        status='down'
+    }else if(e.keyCode==38 && status !='down'){
+        status='up'
+    }else if(e.keyCode==37 && status !='right'){
+        status='left'
+    }else if(e.keyCode==39 && status !='left'){
+        status='right'
+    }
+}
+let xxx = controlArea.offsetLeft;
+let yyy = controlArea.offsetTop;
+function touchSatrtFunc(e){
+    e.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
+    var touch = e.touches[0];   //获取第一个触点
+    var x = Number(touch.pageX); //页面触点X坐标
+    var y = Number(touch.pageY); //页面触点Y坐标
+    startX = x;
+    startY = y;
+    oL = control.offsetLeft;                                 //可拖动元素距离页面左侧的距离
+    oT = control.offsetTop;                                  //可拖动元素距离页面顶部的距离
+    sL = x - oL;           //获取页面触点距离div左侧的距离
+    sT = y - oT;           //获取页面触点距离div顶部的距离
+    console.log(touch,x,y,oL,oT,sL,sT)
+}
+function touchMoveFunc(e){
+    e.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
+    var touch = e.touches[0];   //获取第一个触点
+    var x = Number(touch.pageX); //页面触点X坐标
+    var y = Number(touch.pageY); //页面触点Y坐标
+    let top = y-yyy;
+    let left = x-xxx;
+    if(y-yyy<=15){
+        top=15
+    }else if(y-yyy>85){
+        top=85
+    }
+    if(x-xxx<=15){
+        left=15
+    }else if(x-xxx>85){
+        left=85
+    }
+    if(left<50){
+        if(Math.abs(left-50)>Math.abs(top-50) && status!=='right'){
+            status = 'left'
+        }else if(top<=50 && status!=='down'){
+            status = 'up'
+        }else if(top>=50 && status!=='up'){
+            status = 'down'
+        }
+    }
+    if(left>50){
+        if(Math.abs(left-50)>Math.abs(top-50) && status!=='left'){
+            status = 'right'
+        }else if(top<=50 && status!=='down'){
+            status = 'up'
+        }else if(top>=50 && status!=='up'){
+            status = 'down'
+        }
+    }
+    console.log(status)
+    control.style.top = top + 'px';
+    control.style.left = left + 'px';
+}
+function touchEndFunc(){
+    control.style.top = 50 + 'px';
+    control.style.left = 50 + 'px';
+}
+control.addEventListener("touchstart",touchSatrtFunc,false);
+control.addEventListener("touchmove",touchMoveFunc,false);
+control.addEventListener("touchend",touchEndFunc,false);
+document.addEventListener('keydown',keyDown)
